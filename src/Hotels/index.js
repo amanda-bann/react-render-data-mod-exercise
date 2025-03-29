773ee5be7d9565bb61e6adffb3cc48fb0631eaf3
amanda-bann <amanda.banner@gmail.com>
2025-03-29 11:46:15 -0600

    Render the hotels data model

--- src/Hotels/index.js
+++ src/Hotels/index.js
@@ -1,13 +1,18 @@
 import React from "react";
+// import Hotel from Hotel.js
+import Hotel from "./Hotel.js";
 
-const renderHotel = () => null;
+// Create a callback function to render each hotel
+const renderHotel = (hotel) => <Hotel key={hotel.id} hotel={hotel} />;
 
 const HotelsList = (props) => {
   if (!props.hotels) {
     return null;
   }
-
-  return <ul className="hotels-list">{}</ul>;
+  // Use the map function to map over the hotels prop and call the renderHotel function in the hotels array
+  const hotelListElements = props.hotels.map(renderHotel);
+  // Return the hotel list elements inside the unordered list
+  return <ul className="hotels-list">{hotelListElements}</ul>;
 };
 
 export default HotelsList;
